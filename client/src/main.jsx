00d592b2be3fb07da400fc7ff3eea410a3ab98be
Gerardo Maruotti <maruotti@icloud.com>
2023-11-06 20:16:26 +0100

    Add Auth0 authentication to client package

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { Auth0Provider } from '@auth0/auth0-react';
+import App from './App';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const root = createRoot(document.getElementById('root'));
+
+root.render(
+	<Auth0Provider
+		domain='dev-1v67v8wnwnvivonl.us.auth0.com'
+		clientId='Psju9rPDQ1Ngh4JRo8zFAa4iVVtrV57E'
+		authorizationParams={{
+			redirect_uri: window.location.origin,
+			audience: 'https://dev-1v67v8wnwnvivonl.us.auth0.com/api/v2/',
+			scope: 'read:current_user update:current_user_metadata',
+		}}
+	>
+		<App />
+	</Auth0Provider>
+);
+
+export default Auth0ProviderWithHistory;
