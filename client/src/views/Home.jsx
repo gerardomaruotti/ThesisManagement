827b1f60deb2a51dfce4a892bc1dee3fb7c7ad7e
Skank16 <scampo.andrea@gmail.com>
2023-11-09 11:38:01 +0100

    setup bootstrap color

--- client/src/views/Home.jsx
+++ client/src/views/Home.jsx
@@ -1,12 +1,25 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container, Row, Col, Button, Form, InputGroup } from 'react-bootstrap';
+import '../constants/custom-styles.scss';
 
 
 function Home() {
 
     return (
-        <>
-            <h1>Home</h1>
-        </>
+        <Container>
+            <Row>
+                <Col>
+                    <InputGroup className="mb-3">
+                        <Form.Control
+                            placeholder="Search"
+                        />
+                        <Button variant="outline-primary" id="button-addon2">
+                            Button
+                        </Button>
+                    </InputGroup>
+                </Col>
+            </Row>
+        </Container>
     )
 }
 
