6af01a7553376fe258b1d45a4414c8db5d7088d5
Gerardo Maruotti <maruotti@icloud.com>
2023-11-09 09:39:28 +0100

    Add favicon, colors, and header component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,19 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './views/Home';
-
+import './App.css';
+import Header from './components/Header';
 
 function App() {
-
-  return (
-    <BrowserRouter>
-      {/* <MyNavbar loggedIn={loggedIn} logout={handleLogout} /> */}
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/*' element={<Home />} />
-      </Routes>
-    </BrowserRouter>
-  )
+	return (
+		<BrowserRouter>
+			<Header />
+			<Routes>
+				<Route path='/' element={<Home />} />
+				<Route path='/*' element={<Home />} />
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
-export default App
+export default App;
