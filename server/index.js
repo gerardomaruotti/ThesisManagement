b09bd56c38dde419675442d7455bd16434cc8ec2
jackcauda00 <giacomo.cauda.shop@gmail.com>
2023-11-10 19:17:27 +0100

    definizione API

--- server/index.js
+++ server/index.js
@@ -20,4 +20,84 @@ const corsOptions = {
 };
 app.use(cors(corsOptions));
 
+
+
+
+//METODI API
+//fare metodo gestione autenticazione --> che ritorna ID matricla e chekka auth0
+
+
+//return the thesis corresponding to a professor.
+app.get('/api/thesis/teacher', async (req, res) => {
+	try {
+		//const counters = await db.listOfCounter();
+		res.status(200).json(/*counters*/);
+	} catch (err) {
+		res.status(500).end();
+	}
+});
+
+//return all thesis of the department of the student
+app.get('/api/thesis/student', async (req, res) => {
+	try {
+		//const counters = await db.listOfCounter();
+
+    //gestione dei filtri interna 
+    //separare i casi con filtro e senza filtro 
+		res.status(200).json(/*counters*/);
+	} catch (err) {
+		res.status(500).end();
+	}
+});
+
+//servizio per inserimento --> lista teacher (id, group)
+//servizio per inserimento --> lista groups (id, group)
+app.get('/api/thesis/inert/info', async (req, res) => {
+	try {
+		//const counters = await db.listOfCounter();
+
+    //gestione dei filtri interna 
+    //separare i casi con filtro e senza filtro 
+		res.status(200).json(/*counters*/);
+	} catch (err) {
+		res.status(500).end();
+	}
+});
+
+
+//gestione degli inserimenti nelle varie tabelle secondarie (keyword etc..) 
+app.post('/api/thesis/insert', async (req, res) => {
+	try {
+		//const helpDesk = await db.insertHelpDesk(serviceId, counterList);
+		return res.status(200).json('Inserimento avvenuto con successo');
+	} catch (err) {
+		return res.status(503).json({ error: 'Errore nell inserimento' });
+	}
+});
+
+//ritorniamo le liste di campi necessari per la visualizzazione intera della thesi 
+app.get('/api/thesis/details', async (req, res) => {
+	try {
+		//const counters = await db.listOfCounter();
+
+    //gestione dei filtri interna 
+    //separare i casi con filtro e senza filtro 
+		res.status(200).json(/*counters*/);
+	} catch (err) {
+		res.status(500).end();
+	}
+});
+
+////////////////////////////////////////////////////////////////////
+
+//API for the 3rd story --> Apply for proposal 
+app.post('/api/proposal/insert', async (req, res) => {
+	try {
+		//const helpDesk = await db.insertHelpDesk(serviceId, counterList);
+		return res.status(200).json('Inserimento avvenuto con successo');
+	} catch (err) {
+		return res.status(503).json({ error: 'Errore nell inserimento' });
+	}
+});
+
 module.exports = { app, port };
