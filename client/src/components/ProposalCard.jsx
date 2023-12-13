004fd018bad3c981eab155622ce4cec6f0ac77e9
Skank16 <scampo.andrea@gmail.com>
2023-12-13 19:34:08 +0100

    fix code duplication proposalCard and proposal

--- client/src/components/ProposalCard.jsx
+++ client/src/components/ProposalCard.jsx
@@ -9,6 +9,7 @@ import EditingButtons from './EditingButtons.jsx';
 import { useState } from 'react';
 import { useLoading } from '../LoadingContext.jsx';
 import PropsTypes from 'prop-types';
+import ModalWithUpload from './ModalWithUpload.jsx';
 
 function ProposalCard({
 	accessToken,
@@ -265,34 +266,12 @@ function ProposalCard({
 					)}
 				</Card>
 			</Col>
-			<Modal show={showModal} onHide={() => setShowModal(false)} centered>
-				<Modal.Header closeButton>
-					<Modal.Title>Apply</Modal.Title>
-				</Modal.Header>
-				<Modal.Body>
-					<div>Are you sure you want to apply to this proposal?</div>
-					<div>
-						<br />
-						<Form.Group controlId='formFile' className='mb-3'>
-							<Form.Label>Upload your cv (optional)</Form.Label>
-							<Form.Control type='file' accept='application/pdf' onChange={(e) => setCv(e.target.files[0])} />
-						</Form.Group>
-					</div>
-				</Modal.Body>
-				<Modal.Footer>
-					<Button
-						variant='outline-secondary'
-						onClick={() => {
-							setShowModal(false);
-						}}
-					>
-						No
-					</Button>
-					<Button variant='outline-primary' onClick={apply}>
-						Yes
-					</Button>
-				</Modal.Footer>
-			</Modal>
+			<ModalWithUpload
+				showModal={showModal}
+				setShowModal={setShowModal}
+				apply={apply}
+				setCv={setCv}
+			/>
 		</>
 	);
 }
