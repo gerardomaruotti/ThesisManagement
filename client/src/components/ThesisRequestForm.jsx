d78c62dd5606acdf9d3a38c61a388e0675e59ba8
Gerardo Maruotti <maruotti@icloud.com>
2024-01-09 15:44:29 +0100

    Refactor ThesisRequestForm component to handle form submission

--- client/src/components/ThesisRequestForm.jsx
+++ client/src/components/ThesisRequestForm.jsx
@@ -2,11 +2,21 @@ import PropsTypes from 'prop-types';
 import { useState } from 'react';
 import { Form, Container, Row, Col, Button } from 'react-bootstrap';
 
-function ThesisRequestForm({ accessToken, handleError, request }) {
+function ThesisRequestForm({ accessToken, handleError }) {
 	const [title, setTitle] = useState('');
 	const [description, setDescription] = useState('');
 	const [supervisor, setSupervisor] = useState('');
 	const [cosupervisors, setCosupervisors] = useState([]);
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		const newRequest = {
+			title: title,
+			description: description,
+			supervisor: supervisor,
+			cosupervisors: cosupervisors,
+		};
+	};
 
 	return (
 		<Form>
@@ -14,11 +24,11 @@ function ThesisRequestForm({ accessToken, handleError, request }) {
 				<Col>
 					<Form.Group controlId='supervisor'>
 						<Form.Label>Supervisor</Form.Label>
-						<Form.Control type='text' placeholder='Enter supervisor' value={title} onChange={(e) => setTitle(e.target.value)} />
+						<Form.Control type='text' placeholder='Enter supervisor' value={supervisor} onChange={(e) => setTitle(e.target.value)} />
 					</Form.Group>
 					<Form.Group controlId='cosupervisors'>
 						<Form.Label>Co-supervisors</Form.Label>
-						<Form.Control type='text' placeholder='Enter supervisor' value={title} onChange={(e) => setTitle(e.target.value)} />
+						<Form.Control type='text' placeholder='Enter supervisor' value={cosupervisors} onChange={(e) => setTitle(e.target.value)} />
 					</Form.Group>
 				</Col>
 				<Col>
@@ -28,12 +38,19 @@ function ThesisRequestForm({ accessToken, handleError, request }) {
 					</Form.Group>
 					<Form.Group controlId='description'>
 						<Form.Label>Description</Form.Label>
-						<Form.Control type='text' placeholder='Enter supervisor' value={description} onChange={(e) => setDescription(e.target.value)} />
+						<Form.Control
+							type='text'
+							as='textarea'
+							placeholder='Enter supervisor'
+							rows={3}
+							value={description}
+							onChange={(e) => setDescription(e.target.value)}
+						/>
 					</Form.Group>
 				</Col>
 			</Row>
 			<Container style={{ textAlign: 'center', marginTop: 20 }}>
-				<Button variant='outline-secondary' className='form-button' type='cancel' onClick={() => navigate('/requests')}>
+				<Button variant='outline-secondary' className='form-button' type='cancel' onClick={() => navigate(-1)}>
 					Cancel
 				</Button>
 				<Button variant='primary' className='form-button' type='submit'>
@@ -47,7 +64,6 @@ function ThesisRequestForm({ accessToken, handleError, request }) {
 ThesisRequestForm.propTypes = {
 	accessToken: PropsTypes.string,
 	handleError: PropsTypes.func,
-	request: PropsTypes.object,
 };
 
 export default ThesisRequestForm;
